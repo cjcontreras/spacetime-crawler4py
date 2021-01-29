04b5295300c356e16087d4a4f41e7d576575cf91
Conner Contreras <cjcontreras99@gmail.com>
2021-01-28 18:59:00 -0800

    Found crappy url

--- Test1.py
+++ Test1.py
@@ -8,3 +8,6 @@ from scraper import is_valid, extract_next_links
 if __name__ == '__main__':	
 	url = "https://www.ics.uci.edu/"
 	extract_next_links(url, None)
+
+	url1 = "http://www.informatics.uci.edu/files/pdf/InformaticsBrochure-March2018"
+	extract_next_links(url1, None)
