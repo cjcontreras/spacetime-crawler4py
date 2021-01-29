5102b0705739c0616b9a0d3cc74ea40e1bd364ed
Conner Contreras <cjcontreras99@gmail.com>
2021-01-28 18:30:57 -0800

    added error checking and rejection of some pages

--- Test1.py
+++ Test1.py
@@ -1,6 +1,10 @@
 import re
+import requests
+import cbor
+from utils import response
 from urllib.parse import urlparse
 from scraper import is_valid, extract_next_links
 
 if __name__ == '__main__':	
-	extract_next_links("https://www.ics.uci.edu/", None)
+	url = "https://www.ics.uci.edu/"
+	extract_next_links(url, None)
--- scraper.py
+++ scraper.py
@@ -4,69 +4,98 @@ from urllib.request import *
 from bs4 import BeautifulSoup
 
 def scraper(url, resp):
-    links = extract_next_links(url, resp)
-    return[link for link in links if is_valid(link)]
+	links = extract_next_links(url, resp)
+	return[link for link in links if is_valid(link)]
 
-# TODO:
-#   1) edit is_valid such that domains not included in accepted list is 
-#   rejected - Low Priority
-#   2) 
+# ==================================================
 #
-#
-#
-#
-
+#	list extract_next_links(url, Response obj)
+# 
+# ==================================================
 
 def extract_next_links(url, resp):
-    # get list of urls found after tokenizening the project
-    temp = []
+	
+	# TODO:
+	# Determine if it is worth scraping
 
-    # if resp is bad, return and move on to next query
-    good = [200, 201, 202]
-    if resp.status not in good:
-        return temp
+	# get list of urls found after tokenizening the project
+	temp = []
+	avoid = open("Avoids.txt", 'r')
+	traps = avoid.read()
 
-    page = urlopen(url)
-    soup = BeautifulSoup(page, 'html.parser')
-    for link in soup.find_all('a'):
-        temp.append(link.get('href'))
+	
+	if url in traps:
+		return temp
 
-    return temp
+	# Basically we do not want to look at any pages where there are breaking errors
+	if resp is not None:
+		good = [200, 201, 202]
+		if resp.status not in good:
+			return temp
+
+	# This is in case of the resp being 200 but contains no data. 
+	try:
+		page = urlopen(url)
+	except:
+		return temp
+	else:
+	
+	
+
+	# Assumption is that the page is a safe object to look at
+	# BeautifulSoup takes the page object and converts it into readable HTML
+	# 'a' is speficially a tag in HTML that refers to a new link
+	# link.get('href') returns the value associated with the key
+		soup = BeautifulSoup(page, 'html.parser')
+		if len(soup.get_text()) < 100:
+			return temp
+
+		for link in soup.find_all('a'):
+			temp.append(link.get('href'))
+
+		return temp
+
+
+# ==================================================
+#
+#	bool is_valid(str)
+# 
+# ==================================================
 
 def is_valid(url):
-    try:
-        url = urldefrag(url)
-        parsed = urlparse(url[0])
-        if parsed.scheme not in set(["http", "https"]):
-            return False
+	try:
+		url = urldefrag(url)
+		parsed = urlparse(url[0])
+		if parsed.scheme not in set(["http", "https"]):
+			return False
 
-        valids = set([".ics.uci.edu"
-                     ,".cs.uci.edu"
-                             ,".informatics.uci.edu"
-                             ,".stat.uci.edu"
-                             ,"today.uci.edu"
-                             ])
-        present = False
+		valids = set([".ics.uci.edu"
+					 ,".cs.uci.edu"
+							 ,".informatics.uci.edu"
+							 ,".stat.uci.edu"
+							 ,"today.uci.edu"
+							 ])
+		present = False
 
-        for domain in valids:
-            if domain in parsed.netloc:
-                present = True
-                break
+		for domain in valids:
+			if domain in parsed.netloc:
+				present = True
+				break
 
-        if not present:
-            return False
+		if not present:
+			return False
 
-        # Needs to add more possibilities?
-        return not re.match(
-            r".*\.(css|js|bmp|gif|jpe?g|ico"
-            + r"|png|tiff?|mid|mp2|mp3|mp4"
-            + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
-            + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
-            + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
-            + r"|epub|dll|cnf|tgz|sha1"
-            + r"|thmx|mso|arff|rtf|jar|csv"
-            + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
+		# Needs to add more possibilities?
+		return not re.match(
+			r".*\.(css|js|bmp|gif|jpe?g|ico"
+			+ r"|png|tiff?|mid|mp2|mp3|mp4"
+			+ r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
+			+ r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
+			+ r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
+			+ r"|epub|dll|cnf|tgz|sha1"
+			+ r"|thmx|mso|arff|rtf|jar|csv"
+			+ r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
 
-    except TypeError:
-        print ("TypeError for ", parsed)
-        raise
+	except TypeError:
+		print ("TypeError for ", parsed)
+		raise
