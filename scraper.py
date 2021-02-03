f7cb6cee6c826d2693994a49d9b5c2fee43caa9c
Conner Contreras <cjcontreras99@gmail.com>
2021-02-02 23:07:07 -0800

    second run in development

--- scraper.py
+++ scraper.py
@@ -212,7 +212,7 @@ def is_valid(url):
 			+ r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
 			+ r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
 			+ r"|epub|dll|cnf|tgz|sha1"
-			+ r"|thmx|mso|arff|rtf|jar|csv|war|apk|ppsx"
+			+ r"|thmx|mso|arff|rtf|jar|csv|war|apk|pps|mpg"
 			+ r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
 
 	except TypeError:
